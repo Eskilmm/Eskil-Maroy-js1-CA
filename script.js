addfef079d51a6d144f1714b6b22dcc62d5d3826
Eskilmm <eskilmaroy@gmail.com>
2022-02-28 13:29:50 +0100

    s

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-fetch("https://api-football-v1.p.rapidapi.com/v3/timezone", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-		"x-rapidapi-key": "c0f92a7700mshb2b1fc113862bcap173f6cjsnd4557b553058"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
+fetch("https://fakestoreapi.com/products")
+.then((data)=>{
+    console.log(data);
 });
