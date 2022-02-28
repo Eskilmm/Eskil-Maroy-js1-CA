fb5a28282cd26cfe21dea5f3ce8f4d30d1c62ca3
Eskilmm <eskilmaroy@gmail.com>
2022-02-28 11:27:59 +0100

    checkin

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+fetch("https://api-football-v1.p.rapidapi.com/v3/leagues", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"x-rapidapi-key": "c0f92a7700mshb2b1fc113862bcap173f6cjsnd4557b553058"
+	}
+})
+.then(response => {
+    console.log(response);
+})
+.catch(err =>{
+    console.log(err);
+})
 
